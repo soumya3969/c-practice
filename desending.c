cc11c9f2774f565017156ca9bf0bc3b8ce27efef
soumya <117825720+soumya3969@users.noreply.github.com>
2023-01-10 22:17:15 +0530

    desc.c

---
+++ desending.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("Enter A Number: ");
+	scanf("%d",&n);
+	
+	for(i=n;i>=1;i--)
+	{
+		printf("%d\n",i);
+	}
+	printf("These are the all number in desending number\n");
+	return 0;
+}
