afd060540ad70feba8358facdabe753a870f9d63
soumya <117825720+soumya3969@users.noreply.github.com>
2023-02-10 20:03:15 +0530

    pointer basics-1

---
+++ p-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    int *ptr;
+
+    ptr =&a;
+    printf("%d\n",ptr);
+    printf("%d\n",*ptr);
+    printf("%d\n",&a);
+    printf("%d\n",a);
+    return 0;
+}
