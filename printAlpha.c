ce6d5a5a79f9184ca195b27528cd617559988290
soumya <117825720+soumya3969@users.noreply.github.com>
2023-02-19 17:05:29 +0530

    alphabet print

---
+++ printAlpha.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int ch;
+	printf("Capital Alphabets from A to Z \n");
+	for(ch='A';ch<='Z';ch++){
+	printf("%c\n",ch);	
+	}
+	printf("Capital Alphabets from A to Z \n");
+	for(ch='a';ch<='z';ch++){
+	printf("%c\n",ch);	
+	}
+	return 0;
+}
