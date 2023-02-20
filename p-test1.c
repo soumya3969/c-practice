cb8ea1fdaec2f4d7e484e63606fdaffa04e320f6
soumya <117825720+soumya3969@users.noreply.github.com>
2023-02-20 20:28:45 +0530

    test

---
+++ p-test1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>  
+int main()  
+{  
+    int j = 1;  
+    switch(j)  
+    {  
+        printf("Hello programmer!");  
+        case 1:  
+            printf("Case1");  
+            break;  
+        case 2:  
+            printf("Case2");  
+            break;  
+    }  
+return 0;  
+}
